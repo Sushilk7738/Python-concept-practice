01d6aabe27eb558f2001f66bb6bb8a33e60c1bbc
Sushil Kamble <sushilkamble7738@gmail.com>
2026-01-13 09:06:30 +0000

    learnt and practiced walrus operator in python

---
+++ walrus operator/test.py
@@ -0,0 +1,49 @@
+# this operator is added after version of 3.8 python
+
+# a = 100 
+# print(a + 1)
+
+# print((a = 100)+1)   #it causes error
+
+
+"""
+* You cant assign and evalute at the same time in expression
+* and for that python introduces walrus operator  ":="
+
+You can assign value within an expression which was not possibled earlier
+"""
+# print((a:=100 +1))
+
+
+# a = [10, 20 , 30 , 40 , 50]
+
+# i =  -1
+
+# while (i := i + 1) < (n := len(a)):
+#     print(a[i])
+    
+    
+    
+    
+    
+# data = [10, 20 , 30 , 40 , 50]
+
+# while n:=len(data) > 0 :
+#     print(data.pop())
+#     n -=1
+
+# print(data)
+    
+    
+    
+
+
+# while (ans := input("Do you want to continue? (Y/N): ").lower()):    
+#     if ans != 'y':
+#         break
+
+#     print('Process the request')
+    
+    
+    
+    
