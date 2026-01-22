744132acd25415f0f04e5db5230357da7171c9df
Sushilk <sushilkamble7738@gmail.com>
2026-01-22 14:36:46 +0530

    learnt threading and practiced with some example

---
+++ Multithreading/Thread_communications.py
@@ -0,0 +1,13 @@
+"""
+Thread communication:- 
+In concurrent programming sometimes we need to co-ordinate threads.
+Threads communicate with each othre via signals.
+
+3 ways to communicate:-
+1. by creating event object
+2. by creating condtion object
+3. by using queue module
+"""
+
+# way 1. Using event:- 
+
